0b84169cd947aaca60b7a222a15e65e9f551935c
madalynrose <madalyn.parker+github@gmail.com>
2020-01-09 13:57:49 -0500

    use onClientEntry instead of onInitialClientRender API for gatsby plugin

--- gatsby-plugin-plexiglass/gatsby-browser.js
+++ gatsby-plugin-plexiglass/gatsby-browser.js
@@ -1,8 +1,9 @@
-export const onInitialClientRender = (_, pluginOptions) => {
+export const onClientEntry = (_, pluginOptions) => {
 	if (process.env.NODE_ENV === 'development') {
 		const { noMouseDay, screenOverlay } = pluginOptions;
 		let date = new Date();
 		let dayOfWeek = date.getDay();
+
 		if (!noMouseDay || dayOfWeek === noMouseDay) {
 			import('no-mouse-days');
 			if (screenOverlay === true) {
